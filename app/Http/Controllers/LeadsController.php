ef56e1587bc2be2458e04cbeef8cffc0192e5eab
webastral.development <webastral.development@gmail.com>
2023-05-26 17:47:25 +0530

    first commit

---
+++ app/Http/Controllers/LeadsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class LeadsController extends Controller
+{
+    public function getRecentProspects()
+    {
+        $accessToken = env('ACCESS_TOKEN');
+
+        $client = new Client();
+
+        try {
+            $response = $client->request('GET', 'https://sandbox.zohoapis.com.au/crm/v3/Prospects', [
+                'headers' => [
+                    'Authorization' => 'Zoho-oauthtoken ' . $accessToken,
+                    'Content-Type' => 'application/json'
+                ],
+                'query' => [
+                    'sort_by' => 'Created_Time',
+                    'sort_order' => 'desc',
+                    'page' => 1,
+                    'per_page' => 5,
+                    'fields' => '*(*)' // Include all fields for the prospects
+                ]
+            ]);
+
+            $prospects = json_decode($response->getBody(), true)['data'];
+          $data = $this->getProspectusById($prospects);
+            return response()->json($data);
+
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function getProspectusById($prospects){
+
+        $accessToken = env('ACCESS_TOKEN');
+        $data=array();
+        foreach ($prospects as $id){
+            $client = new Client();
+            $response = $client->request('GET', 'https://sandbox.zohoapis.com.au/crm/v3/Prospects/'.$id['id'], [
+                'headers' => [
+                    'Authorization' => 'Zoho-oauthtoken ' . $accessToken,
+                    'Content-Type' => 'application/json'
+                ],
+                'query' => [
+                    'sort_by' => 'Created_Time',
+                    'sort_order' => 'desc',
+                    'page' => 1,
+                    'per_page' => 5,
+                ]
+            ]);
+
+            $data[] = json_decode($response->getBody(), true)['data'];
+
+        }
+        return $data;
+    }
+   //Create Prospectus
+    public function createProspect(Request $request)
+    {
+        $accessToken = env('ACCESS_TOKEN');
+
+        $client = new Client();
+
+        try {
+            $response = $client->request('POST', 'https://sandbox.zohoapis.com.au/crm/v3/Leads', [
+                'headers' => [
+                    'Authorization' => 'Zoho-oauthtoken ' . $accessToken,
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => [
+                    'data' => [
+                        [
+                            'First_Name' => $request->input('first_name'),
+                            'Last_Name' => $request->input('last_name'),
+                            // Include other required and optional fields for the prospect
+                        ]
+                    ]
+                ]
+            ]);
+
+            $prospect = json_decode($response->getBody(), true)['data'][0];
+            // Send email with the response data
+            $to = 'it@truewealth.com.au';
+            $subject = 'New Prospectus';
+            $message = 'A new prospectus has been created.' . PHP_EOL . PHP_EOL;
+            $message .= 'Prospect Details:' . PHP_EOL;
+            $message .= 'First Name: ' . $prospect['First_Name'] . PHP_EOL;
+            $message .= 'Last Name: ' . $prospect['Last_Name'] . PHP_EOL;
+            // Include other relevant prospect details
+
+            // Send email using the mail() function
+            mail($to, $subject, $message);
+
+            return response()->json($prospect, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+
+}
